e59fe634487d89fc6802dceaa47aabbe4eaa5801
PatrikG <Patrik0Gn@gmail.com>
2021-08-11 15:05:33 +0200

    Add basic Function to create and render cells

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Cell from "./Cell.js";
 
 function App() {
-  return <div className="App"></div>;
+  const numbers = [8, 3, 2];
+
+  function renderCells() {
+    const numbArray = numbers.map((number, index) => {
+      return <Cell key={index} value={number} />;
+    });
+    return numbArray;
+  }
+
+  return <div className="App">{renderCells()}</div>;
 }
 
 export default App;
