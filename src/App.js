e3172273bb31e5039c6c17c65acd19ca3e91430a
PatrikG <Patrik0Gn@gmail.com>
2021-08-12 11:56:37 +0200

    Add sample Sudoku array

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import "./App.css";
 import Cell from "./Cell.js";
 
 function App() {
-  const numbers = [8, 3, 2];
+  const sampleGrid = [
+    [0, 1, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 6, 7, 0, 9],
+    [0, 7, 0, 0, 3, 0, 0, 4, 0],
+    [0, 2, 3, 0, 0, 0, 0, 9, 0],
+    [0, 0, 0, 0, 0, 1, 0, 2, 7],
+    [7, 0, 0, 3, 0, 0, 0, 0, 0],
+    [3, 4, 1, 6, 8, 5, 0, 7, 0],
+    [5, 6, 7, 0, 2, 0, 8, 0, 0],
+    [0, 0, 2, 0, 1, 0, 0, 3, 0],
+  ];
 
   function renderCells() {
     const numbArray = numbers.map((number, index) => {
