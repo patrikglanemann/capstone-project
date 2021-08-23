60a0bf5785061553cb3cf2c50e73c108ee53c7af
PatrikG <Patrik0Gn@gmail.com>
2021-08-23 14:46:19 +0200

    Add a header to the main page and display icons

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,23 @@
+import { NavLink } from "react-router-dom";
+import { ReactComponent as ProfileIcon } from "../svg/profile-fill.svg";
+import { ReactComponent as HighscoreIcon } from "../svg/chart-fill.svg";
+import { ReactComponent as OptionsIcon } from "../svg/gear-fill.svg";
+
 export default function MainPage() {
   return (
     <div className="MainPage">
+      <header>
+        <div className="Header__topImage">
+          <h1>Main</h1>
+        </div>
+        <nav className="Header__NavBar">
+          <ProfileIcon />
+          <NavLink to="/highscores">
+            <HighscoreIcon />
+          </NavLink>
+          <OptionsIcon />
+        </nav>
+      </header>
       <main>Main Page</main>
     </div>
   );
