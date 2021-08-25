b52393955611be15d5d8c2c43859d01ecf2725e8
PatrikG <Patrik0Gn@gmail.com>
2021-08-26 01:22:13 +0200

    Add switch and route to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,49 @@
 import "./Header.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, Switch, Route } from "react-router-dom";
 import { ReactComponent as ProfileIcon } from "../svg/profile-fill.svg";
 import { ReactComponent as HighscoreIcon } from "../svg/chart-fill.svg";
 import { ReactComponent as OptionsIcon } from "../svg/gear-fill.svg";
 
-export default function Header({ title, subTitle, type }) {
+export default function Header() {
   return (
     <header className="Header">
-      <div className="Header__topImage">
-        <h1 className="Header__title">{title}</h1>
-        <h2 className="Header__subTitle">{subTitle}</h2>
-      </div>
-      <nav
-        className={`Header__NavBar ${type === "1" && "Header__NavBar--active"}`}
-      >
-        <NavLink to="/profile">
-          <ProfileIcon />
-        </NavLink>
-        <NavLink to="/highscores">
-          <HighscoreIcon />
-        </NavLink>
-        <OptionsIcon />
-      </nav>
+      <Switch>
+        <Route path="/highscore">
+          <div className="Header__topImage">
+            <h1 className="Header__title">Highscore</h1>
+          </div>
+        </Route>
+        <Route path="/map">
+          <div className="Header__topImage">
+            <h1 className="Header__title">Map</h1>
+          </div>
+        </Route>
+        <Route path="/sudoku/summary">
+          <div className="Header__topImage">
+            <h1 className="Header__title">Victory</h1>
+          </div>
+        </Route>
+        <Route path="/sudoku">
+          <div className="Header__topImage">
+            <h1 className="Header__title">Room01</h1>
+            <h2 className="Header__subTitle">easy</h2>
+          </div>
+        </Route>
+        <Route path="/">
+          <div className="Header__topImage">
+            <h1 className="Header__title">Main</h1>
+          </div>
+          <nav className={"Header__navBar"}>
+            <NavLink to="/profile">
+              <ProfileIcon />
+            </NavLink>
+            <NavLink to="/highscore">
+              <HighscoreIcon />
+            </NavLink>
+            <OptionsIcon />
+          </nav>
+        </Route>
+      </Switch>
     </header>
   );
 }
