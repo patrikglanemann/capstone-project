17dd11e7391f85b4c848b1af9fb9726692452bdc
PatrikG <Patrik0Gn@gmail.com>
2021-08-26 19:17:03 +0200

    Change value of key prop

--- src/pages/HighscorePage.js
+++ src/pages/HighscorePage.js
@@ -4,9 +4,9 @@ export default function highscorePage() {
   const highscores = [10, 50, 25, 35, 0, 85, 40];
 
   function renderHighscores() {
-    const highscoreList = highscores.map((highscore) => {
+    const highscoreList = highscores.map((highscore, index) => {
       return (
-        <li key={Date.now()} className="highscore__item">
+        <li key={index} className="highscore__item">
           {highscore}
         </li>
       );
