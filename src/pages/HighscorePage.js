520dcec72e476f179866c243db686d5ac69c3620
PatrikG <Patrik0Gn@gmail.com>
2021-08-26 12:29:12 +0200

    Create basic highscore list items

--- src/pages/HighscorePage.js
+++ src/pages/HighscorePage.js
@@ -1,3 +1,22 @@
-export default function HighscorePage() {
-  return <p>Highscore</p>;
+import "./HighscorePage.css";
+
+export default function highscorePage() {
+  const highscores = [10, 50, 25, 35, 0, 85, 40];
+
+  function renderHighscores() {
+    const highscoreList = highscores.map((highscore) => {
+      return (
+        <li key={Date.now()} className="highscore__item">
+          {highscore}
+        </li>
+      );
+    });
+    return highscoreList;
+  }
+
+  return (
+    <main className="HighscorePage Content">
+      <ul className="highscoreList">{renderHighscores()}</ul>
+    </main>
+  );
 }
