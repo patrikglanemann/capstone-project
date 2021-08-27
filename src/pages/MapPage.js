ef65d5e7bd3328e5b6aa457250a807ec3a0bc6f1
PatrikG <Patrik0Gn@gmail.com>
2021-08-28 00:04:07 +0200

    Create basic map page

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,3 +1,21 @@
+import "./MapPage.css";
+
 export default function MapPage() {
-  return <main className="MapPage Content">Map</main>;
+  return (
+    <main className="MapPage Content">
+      <button className="MapPage__arrowBtn MapPage__arrowBtn--up"></button>
+      <div className="MapPage__middle">
+        <button className="MapPage__arrowBtn MapPage__arrowBtn--left"></button>
+        <div className="Room">
+          <span>
+            <h3>Difficulty:</h3>
+            <h3>easy</h3>
+          </span>
+          <h3>Points + 5</h3>
+        </div>
+        <button className="MapPage__arrowBtn MapPage__arrowBtn--right"></button>
+      </div>
+      <button className="MapPage__arrowBtn MapPage__arrowBtn--down"></button>
+    </main>
+  );
 }
