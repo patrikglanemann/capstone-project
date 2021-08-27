3ac8b7376a257fc16600b90415904077ff3c1d31
PatrikG <Patrik0Gn@gmail.com>
2021-08-27 10:18:09 +0200

    Add elements to summary page

--- src/pages/SummaryPage.js
+++ src/pages/SummaryPage.js
@@ -1,3 +1,21 @@
+import "./SummaryPage.css";
+import { Link } from "react-router-dom";
+
 export default function SummaryPage() {
-  return <main className="SummaryPage Content">Summary</main>;
+  return (
+    <main className="SummaryPage Content">
+      <div className="Summary__banner">
+        <h2 className="Summary__title">Victory</h2>
+      </div>
+      <div className="Summary__contentBox">
+        <h3>Difficulty:</h3>
+        <h3>easy</h3>
+        <h3>Points + 5</h3>
+        <h3>Score: 85</h3>
+        <Link to="/map">
+          <button className="Summary__doneBtn">Done</button>
+        </Link>
+      </div>
+    </main>
+  );
 }
